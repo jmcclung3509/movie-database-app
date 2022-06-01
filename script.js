722cd725c471b19f4083060dd7e43ebb98b188ca
Jessica Turner <jmcclung3509@gmail.com>
2022-06-01 09:38:29 -0700

    Added hide/show on movie data when image is clicked

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ let starClicked = 0
 
 function getMovieData() {
     container.innerHTML = ""
-    fetch("https://api.themoviedb.org/3/discover/movie?api_key=6af57a5cf47289dd6788043a2cc7d90d&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate")
+    fetch("https://api.themoviedb.org/3/movie/popular?api_key=6af57a5cf47289dd6788043a2cc7d90d&language=en-US&page=1")
+        // fetch("https://api.themoviedb.org/3/discover/movie?api_key=6af57a5cf47289dd6788043a2cc7d90d&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate")
         .then((response) => response.json())
         .then((data) => {
             // let allMovies = data
@@ -23,21 +24,37 @@ function getMovieData() {
                 const overview = item.overview
                 const image = item.backdrop_path
                 const releaseDate = item.release_date
+                const genreArray = item.genre_ids
+                let isKidFriendly = false
+                const genre = genreArray.forEach((item) => {
 
+                    if (item === 10751) {
+                        isKidFriendly = true
+
+                    }
+                })
 
 
                 const newMovie = document.createElement("div")
                 container.appendChild(newMovie)
                 newMovie.classList.add("newMovie")
 
+
                 const movieImg = document.createElement("img")
                 movieImg.src = `https://www.themoviedb.org/t/p/w220_and_h330_face/${image}`
                 newMovie.appendChild(movieImg)
                 movieImg.classList.add("image")
+                movieImg.onclick = () => {
+                    showData()
+                }
+                let movieDataContainer = document.createElement("div")
+                movieDataContainer.classList.add("hide")
+                movieDataContainer.classList.add("data-container")
+                newMovie.appendChild(movieDataContainer)
 
                 let newMovieTitle = document.createElement("h3")
                 newMovieTitle.innerHTML = `${title}`
-                newMovie.appendChild(newMovieTitle)
+                movieDataContainer.appendChild(newMovieTitle)
 
                 let newMovieDate = document.createElement("h5")
                 let newDate = item.release_date.split("-")
@@ -47,33 +64,43 @@ function getMovieData() {
                     let month = newDate[2]
                     let day = newDate[1]
                     dateArray.push(day + "-" + month + "-" + year)
-                    console.log(dateArray)
+
 
                 }
-
                 newMovieDate.innerHTML = dateArray
+                newMovieDate.classList.add("date")
+                movieDataContainer.appendChild(newMovieDate)
 
-                newMovie.appendChild(newMovieDate)
+                if (isKidFriendly) {
+                    let kidFriendly = document.createElement("p")
+                    kidFriendly.classList.add("kid-friendly")
+                    kidFriendly.innerHTML = "Kid Friendly"
+                    movieDataContainer.appendChild(kidFriendly)
+                }
 
                 let newMovieOverview = document.createElement("p")
                 newMovieOverview.innerHTML = `${overview}`
-                newMovie.appendChild(newMovieOverview)
+                movieDataContainer.appendChild(newMovieOverview)
                 newMovieOverview.classList.add("paragraph")
 
+
+
+
+
                 let checkboxContainer = document.createElement("div")
-                newMovie.appendChild(checkboxContainer)
+                movieDataContainer.appendChild(checkboxContainer)
                 checkboxContainer.classList.add("checkbox-container")
 
                 let checkBtn = document.createElement("input")
                 checkBtn.type = "checkbox"
 
                 checkBtn.onclick = () => {
-                    if (starContainer.classList.contains("hide")) {
-                        starContainer.classList.remove("hide")
+                    if (starContainer.classList.contains("hide1")) {
+                        starContainer.classList.remove("hide1")
                         starContainer.classList.add("show")
                     } else {
                         starContainer.classList.remove("show")
-                        starContainer.classList.add("show")
+                        starContainer.classList.add("hide1")
                     }
                 }
 
@@ -84,127 +111,180 @@ function getMovieData() {
 
                 let starContainer = document.createElement("div")
                 checkboxContainer.appendChild(starContainer)
-                starContainer.classList.add("hide")
-
+                starContainer.classList.add("hide1")
 
 
                 let star1 = document.createElement("input")
                 star1.type = "checkbox"
                 star1.classList.add("star-check")
-
                 starContainer.appendChild(star1)
-                star1.onclick = () => {
-                    if (!star1.checked) {
-                        console.log("clicked")
-                        !star1.checked
-                        rating.innerText = "0/5"
-                        star1.classList.add(".star-check:checked:before")
-                    }
-                    star1.checked
-                    rating.innerHTML
-                        = "1/1"
-                }
 
                 let star2 = document.createElement("input")
                 star2.type = "checkbox"
                 starContainer.appendChild(star2)
                 star2.classList.add("star-check")
-                star2.onclick = () => {
-                    if (star2.classList.contains("star-check:checked")) {
-                        rating.innerHTML = "1/5"
-                        !star2.checked
-                    }
-                    star1.checked = true
-                    star2.checked = true
 
-                    rating.innerHTML = "2/5"
-                }
+
 
                 let star3 = document.createElement("input")
                 star3.type = "checkbox"
                 starContainer.appendChild(star3)
                 star3.classList.add("star-check")
-                star3.onclick = () => {
-                    if (star3.checked = true) {
-                        star3.checked = false
-                        // star2.classList.add("star-check:before")
-                        rating.innerHTML = "2/5"
-                    }
-                    star1.checked = true
-                    star2.checked = true
-                    star3.checked = true
-                    rating.innerHTML = "3/5"
-                }
 
                 let star4 = document.createElement("input")
                 star4.type = "checkbox"
                 starContainer.appendChild(star4)
                 star4.classList.add("star-check")
-                star4.onclick = () => {
-                    if (star4.checked = true) {
-                        star4.checked = false
-                        // star2.classList.add("star-check:before")
-                        rating.innerHTML = "3/5"
-                    }
-                    star1.checked = true
-                    star2.checked = true
-                    star3.checked = true
-                    star4.checked = true
-                    rating.innerHTML = "4/5"
-                }
+
                 let star5 = document.createElement("input")
                 star5.type = "checkbox"
                 starContainer.appendChild(star5)
                 star5.classList.add("star-check")
-                star5.onclick = () => {
-                    if (star5.checked = true) {
-                        star5.checked = false
-                        // star2.classList.add("star-check:before")
-                        rating.innerHTML = "4/5"
-                    }
-                    star5.checked = true
-                    star1.checked = true
-                    star2.checked = true
-                    star3.checked = true
-                    star4.checked = true
 
-                    rating.innerHTML = "5/5"
+
+
+                star1.onclick = () => {
+                    if (!star1.checked) {
+                        console.log("clicked")
+                        star1.checked = false
+                        starClicked--
+                        // rating.innerText = "0/5"
+                    } else {
+                        star1.checked = true;
+                        starClicked++
+                        // rating.innerHTML = "1/1"
+                    }
+                    getRating()
+
+                }
+
+
+                star2.onclick = () => {
+                    if (!star2.checked) {
+                        console.log("clicked")
+                        star2.checked = false
+                        starClicked--
+                    } else if (starClicked < 2) {
+                        starClicked = 2
+                        star2.checked = true
+                        star1.checked = true
+
+                    } else {
+                        star2.checked = true;
+                        star1.checked = true
+                        starClicked++
+                    }
+                    getRating()
+
+                }
+
+
+                star3.onclick = () => {
+                    if (!star3.checked) {
+                        console.log("clicked")
+                        star3.checked = false
+                        starClicked--
+                    } else if (starClicked < 3) {
+                        starClicked = 3
+                        star3.checked = true
+                        star2.checked = true
+                        star1.checked = true
+
+                    } else {
+                        star3.checked = true;
+                        star2.checked = true;
+                        star1.checked = true
+                        starClicked++
+
+                    }
+                    getRating()
+
+                }
+
+
+
+                star4.onclick = () => {
+                    if (!star4.checked) {
+                        star4.checked = false;
+                        starClicked--
+                    } else if (starClicked < 4) {
+                        starClicked = 4
+                        star4.checked = true
+                        star3.checked = true
+                        star2.checked = true
+                        star1.checked = true
+
+                    } else {
+                        star4.checked = true
+                        star3.checked = true
+                        star2.checked = true
+                        star1.checked = true
+                        starClicked++
+                    }
+                    getRating()
+                }
+
+
+                star5.onclick = () => {
+                    if (!star5.checked) {
+                        star5.checked = false;
+                        starClicked--
+                    } else if (starClicked < 5) {
+                        console.log("clicked")
+                        star5.checked = true
+                        star4.checked = true
+                        star3.checked = true
+                        star2.checked = true
+                        star1.checked = true
+                        starClicked = 5
+
+                    } else {
+                        star5.checked = true
+                        star4.checked = true
+                        star3.checked = true
+                        star2.checked = true
+                        star1.checked = true
+                        starClicked++
+                    }
+                    getRating()
                 }
 
                 let rating = document.createElement("p")
-                rating.innerHTML = "0/5"
-                rating.classList.add("rating")
                 starContainer.appendChild(rating)
+                rating.classList.add("rating")
+                // rating.innerHTML = `${starClicked}/5`
 
 
+                function getRating() {
+                    rating.innerHTML = `${starClicked}/5`
+                }
+                function showData() {
+                    if (movieDataContainer.classList.contains("hide")) {
+                        movieDataContainer.classList.remove("hide")
+                        movieDataContainer.classList.add("show")
+                    } else {
+                        movieDataContainer.classList.remove("show")
+                        movieDataContainer.classList.add("hide")
+                        hideStarContainer()
 
 
-            })
+                    }
+                }
+
+                function hideStarContainer() {
+                    checkboxContainer.classList.remove("show")
+                    checkboxContainer.classList.add("hide1")
+                }
+
+
+            }
+            )
 
         })
 
-    // function getRating() {
-    //     if (starContainer.classList.contains("hide")) {
-    //         starContainer.classList.remove("hide")
-    //         starContainer.classList.add("show")
 
-    //     } else {
-    //         starContainer.classList.remove("show")
-    //         starContainer.classList.add("show")
-    //     }
-    // }
 
 }
-
-// function getRating() {
-//     if (starContainer.classList.contains("hide")) {
-//         starContainer.classList.remove("hide")
-//         starContainer.classList.add("show")
-
-//     } else {
-//         starContainer.classList.remove("show")
-//         starContainer.classList.add("show")
-//     }
 
 function shuffle(array) {
     let currentIndex = array.length, randomIndex;
@@ -217,9 +297,4 @@ function shuffle(array) {
     return array
 }
 
-
-// function loadMovie(id) {
-//     let allMovies = getMovieData("https://api.themoviedb.org/3/movie/now_playing?api_key=6af57a5cf47289dd6788043a2cc7d90d&language=en-US&page=1");
-
-// }
 
